b375f9d60b3b9a853f581c61759d092ff2587950
thienquy05 <thienquypham05@gmail.com>
2026-04-12 10:38:05 -0400

    feat: Add Vercel Analytics and Speed Insights to enhance performance tracking

--- portfolio-next/src/app/layout.tsx
+++ portfolio-next/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ChatWidget from "@/components/ChatWidget";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +45,7 @@ export default function RootLayout({
       >
         {children}
         <ChatWidget />
+        <Analytics />
       </body>
     </html>
   );
